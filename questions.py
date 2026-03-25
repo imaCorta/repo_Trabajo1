907c179985c3eb72be59dcb562241b3161f1a89b
imaCorta <imanolcortabitarte@gmail.com>
2026-03-25 20:41:20 -0300

    Ya lo modifique para poder jugar varias rondas sin repetir palabra. Tambien implementé una mejora en el sistema de puntos para contabilizar el total en todas las rondas. Se juega hasta terminar con las palabras de la categoría elegida

--- questions.py
+++ questions.py
@@ -22,59 +22,82 @@ guessed = []
 attempts = 6
 
 print("¡Bienvenido al Ahorcado!")
-tema = input("""Seleccione una categoría para jugar
+
+tema = input("""Seleccione una categoría para jugar:
+             -tipo
+             -herramienta
+             -estructura
+             Su elección: """).lower().strip()
+while tema not in categorias:                               # Mod 3
+    print()
+    tema = input("""Categoria invalida.
+Seleccione una categoría para jugar:
              -tipo
              -herramienta
              -estructura
              Su elección: """)
-word = random.choice(categorias[tema])                      # Mod 3
+    
+words = random.sample(categorias[tema], len(categorias[tema]))     
 
 print()
 points = 0
+total_points = 0
 
-while attempts > 0:
-    # Mostrar progreso: letras adivinadas y guiones para las que faltan
-    progress = ""
-    for letter in word:
-        if letter in guessed:
-            progress += letter + " "
+for word in words:
+
+    while attempts > 0:
+        # Mostrar progreso: letras adivinadas y guiones para las que faltan
+        progress = ""
+        for letter in word:
+            if letter in guessed:
+                progress += letter + " "
+            else:
+                progress += "_ "
+        print(progress)
+        print()
+
+        # Verificar si el jugador ya adivinó la palabra completa
+        if "_" not in progress:
+            print("¡Ganaste!")
+
+            points +=6
+            print(f"Obtuviste {points} puntos en esta ronda")                 # Mod 2
+
+            break
+
+        print(f"Intentos restantes: {attempts}")
+        print(f"Letras usadas: {', '.join(guessed)}")
+        print()
+
+        letter = input("Ingresá una letra: ").lower()
+        if len(letter) != 1 or letter not in string.ascii_lowercase:
+            print("Entrada no válida")                          # Mod 1
+
+        elif letter in guessed:
+            print("Ya usaste esa letra.")
+        elif letter in word:
+            guessed.append(letter)
+            print("¡Bien! Esa letra está en la palabra.")
         else:
-            progress += "_ "
-    print(progress)
-    print()
+            guessed.append(letter)
+            attempts -= 1
+            print("Esa letra no está en la palabra.")
 
-    # Verificar si el jugador ya adivinó la palabra completa
-    if "_" not in progress:
-        print("¡Ganaste!")
+            points -= 1                                         # Mod 2
 
-        points +=6
-        print(f"Obtuviste {points} puntos")                 # Mod 2
-
-        break
-
-    print(f"Intentos restantes: {attempts}")
-    print(f"Letras usadas: {', '.join(guessed)}")
-    print()
-
-    letter = input("Ingresá una letra: ").lower()
-
-    if letter not in string.ascii_lowercase:
-        print("Entrada no válida")                          # Mod 1
-
-    elif letter in guessed:
-        print("Ya usaste esa letra.")
-    elif letter in word:
-        guessed.append(letter)
-        print("¡Bien! Esa letra está en la palabra.")
+        print()
     else:
-        guessed.append(letter)
-        attempts -= 1
-        print("Esa letra no está en la palabra.")
+        print(f"¡Perdiste! La palabra era: {word}")
 
-        points -= 1                                         # Mod 2
+        print("Ha obtenido 0 puntos en esta ronda")                           # Mod 2
+        points = 0
 
+    total_points += points
+    points = 0
+    attempts = 6
+    guessed = []
     print()
-else:
-    print(f"¡Perdiste! La palabra era: {word}")
+    if word != words[-1]:
+        print("Ahora otra palabra: ")
 
-    print("Ha obtenido 0 puntos")                           # Mod 2
+print(f"Terminó el juego con {total_points} puntos")
